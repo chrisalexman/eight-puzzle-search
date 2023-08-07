a6c19dd0e4d6fdc282398ee50dfa39c24f61b2bd
Christopher Alexman <christopheralexman@gmail.com>
2023-08-06 22:24:29 -0700

    Add tree class

---
+++ tree.py
@@ -0,0 +1,25 @@
+"""
+
+"""
+
+class Tree:
+
+    def __init__(self):
+        self.node = None        # the node
+        self.parent = None      # the parent of the node
+
+    # set the node
+    def set_node(self, node):
+        self.node = node
+
+    # set the parent of the node
+    def set_parent(self, parent):
+        self.parent = parent
+
+    # get the node
+    def get_node(self):
+        return self.node
+
+    # get the parent of the node
+    def get_parent(self):
+        return self.parent
